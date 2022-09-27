0c93d2f5929e1301ff80b1a53fc9892adff9ab9c
Shannon Wooten <swooten7448@gmail.com>
2022-09-27 09:51:54 -0500

    Completed Exercise

---
+++ script.js
@@ -0,0 +1,71 @@
+// 1
+console.log(3);
+
+
+// 2
+/* 
+Number
+String
+Boolean
+Null
+Undefined
+*/
+
+// 3
+/*
+Addition is +
+Subtration is -
+Muliplication is *
+Division is /
+Modulus is %
+Exponentiation is **
+*/
+
+// 4
+console.log(5 % 2); //1
+console.log(5 % 4); //1
+
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL');
+
+// 6
+console.log("Hawk" + "eye");
+
+// 7
+console.log("Shannon".length + "Wooten".length); //13
+
+// 8
+console.log("Avengers: Endgame"[16]);
+
+// BONUS
+// 1
+console.log("\/");
+
+// 2
+console.log(5 ** 4 - 9);
+
+// 3
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+// 1 - 2 + 4 - 3 = 0
+
+// 4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// 5
+var m = 25;
+var c = 4;
+var u = m + c;
+console.log(u);
+
+// WITH LET
+// let m = 25;
+// let c = 4;
+// let u = m + c;
+// console.log(u);
